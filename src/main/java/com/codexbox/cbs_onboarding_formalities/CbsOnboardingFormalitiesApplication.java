c032819a02dd7134bf496c653eed4377ab678eb0
chandan <barnwalckb@gmail.com>
2022-07-09 13:49:22 +0530

    initial commit for onboarding formalities

---
+++ src/main/java/com/codexbox/cbs_onboarding_formalities/CbsOnboardingFormalitiesApplication.java
@@ -0,0 +1,13 @@
+package com.codexbox.cbs_onboarding_formalities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsOnboardingFormalitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbsOnboardingFormalitiesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/codexbox/cbs_onboarding_formalities/controller/CbsOnboardingFormalitiesController.java
@@ -0,0 +1,13 @@
+package com.codexbox.cbs_onboarding_formalities.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CbsOnboardingFormalitiesController {
+/*
+   @
+
+    }
+}
+*/
+}
