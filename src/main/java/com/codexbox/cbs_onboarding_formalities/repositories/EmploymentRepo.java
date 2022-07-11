a2f4bbc4ef1eeda1de59440af20d5d2caa7ed359
jadduleelaganesh <jadduleelaganesh@gmail.com>
2022-07-11 19:23:09 +0530

    get api & personaldetails

--- src/main/java/com/codexbox/cbs_onboarding_formalities/repositories/EmploymentRepo.java
+++ src/main/java/com/codexbox/cbs_onboarding_formalities/repositories/EmploymentRepo.java
@@ -1,12 +1,9 @@
 package com.codexbox.cbs_onboarding_formalities.repositories;
 
-import com.codexbox.cbs_onboarding_formalities.entities.PersonalDetailsEntity;
 import com.codexbox.cbs_onboarding_formalities.entities.EmploymentDetails;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonalDetailsRepo extends CrudRepository<PersonalDetailsEntity,Integer>{
-
-
+public interface EmploymentRepo extends CrudRepository<EmploymentDetails,Integer> {
 }
