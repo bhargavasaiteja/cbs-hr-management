ce861c30f499744cff990b8a8abb662f50ef4744
CodexBox <smrutiranjan30@gmail.com>
2022-07-12 13:19:11 +0530

    new changes on skill entity table name

--- src/main/java/com/codexbox/employeeIdcreation/entity/SkillsEntity.java
+++ src/main/java/com/codexbox/employeeIdcreation/entity/SkillsEntity.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "skill")
+@Table(name = "skills")
 public class SkillsEntity {
     @Id
     @Column(name = "skill_id")
